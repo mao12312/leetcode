84d7380de2296d3208aed548a197cb90d6badd42
mao12312 <42489486+mao12312@users.noreply.github.com>
2020-01-23 20:26:24 +0900

    add proglem

--- decompress-run-length-encoded-list.js
+++ decompress-run-length-encoded-list.js
@@ -1,3 +1,11 @@
-const functionNmae = (argument) => {
-
+const decompressRLElist = (nums) => {
+    let res = [];
+    for (let i = 0; i < nums.length; i += 2) {
+        while(nums[i]>0){
+            res.push(nums[i+1]);
+            nums[i]--;
+        }
+    }
+    return res;
 }
+decompressRLElist([1, 2, 3, 4])
